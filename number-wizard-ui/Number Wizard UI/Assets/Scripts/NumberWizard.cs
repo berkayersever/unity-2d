9cd977a3fcc05fa26059dc37b827949450935fe9
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-02-23 00:13:05 +0300

    Removed the Update method.

--- number-wizard-ui/Number Wizard UI/Assets/Scripts/NumberWizard.cs
+++ number-wizard-ui/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -31,21 +31,6 @@ public class NumberWizard : MonoBehaviour {
         NextGuess();
     }
 
-    // Update is called once per frame
-    void Update () {		
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {            // Detect when the up arrow key is pressed down
-            min = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) {     // Detect when the down arrow key is pressed down
-            max = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return)) {        // Detect when the up enter key is pressed down
-            StartGame();
-        }
-    }
-
     void NextGuess () {
         guess = (max + min) / 2;
     }
