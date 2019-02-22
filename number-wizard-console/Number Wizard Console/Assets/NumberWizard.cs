e2aba8c204610c105b8303de329e209c8fedeee9
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-02-23 00:01:24 +0300

    Removed [SerializeField] from int guess.

--- number-wizard-console/Number Wizard Console/Assets/NumberWizard.cs
+++ number-wizard-console/Number Wizard Console/Assets/NumberWizard.cs
@@ -6,7 +6,7 @@ public class NumberWizard : MonoBehaviour {
 
     [SerializeField] int min;
     [SerializeField] int max;
-    [SerializeField] int guess;
+    int guess;
 
 	// Use this for initialization
 	void Start () {
